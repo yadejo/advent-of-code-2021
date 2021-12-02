e05d1b48f1a64079a03a01fd9d2cb9be927c9e93
DE JONGHE Yannick (YDEJ) <yannick.dejonghe@attentia.be>
2021-12-02 09:43:27 +0100

    day2 statemachine

--- day2/index.js
+++ day2/index.js
@@ -1,47 +1,46 @@
 const fs = require("fs");
 
 const file = fs.readFileSync('input.txt');
-const lines = file.toString().split('\r\n').map(x => x.split(' '));
+const commands = file.toString().split('\r\n').map(x => x.split(' ')).map(x => [x[0], Number(x[1])]);
 
-// Part 1
-const pos = lines.reduce((acc, curr) => {
-        console.log(acc, curr); 
-        switch (curr[0]) {
+const defaultState = { depth: 0, hor: 0, aim: 0 };
+
+const part1Reducer = (state, [type, number]) => {
+        switch (type) {
                 case 'forward':
-                        return {...acc, hor: acc.hor + parseInt(curr[1]) };
+                        return { ...state, hor: state.hor + number };
                 case 'down':
-                        return {...acc, depth: acc.depth + parseInt(curr[1]) };
+                        return { ...state, depth: state.depth + number };
                 case 'up':
-                        return {...acc, depth: acc.depth - parseInt(curr[1]) };
+                        return { ...state, depth: state.depth - number };
                 default:
-                        throw "boem"
+                        return state;
         }
-        
-}, {depth: 0, hor: 0});
 
+}
 
-console.log(pos.depth * pos.hor);
-
-
-// Part 2
-const pos = lines.reduce((acc, curr) => {
-        console.log(acc, curr); 
-        switch (curr[0]) {
+const part2Reducer = (state, [type, number]) => {
+        switch (type) {
                 case 'forward':
                         return {
-                                ...acc, 
-                                hor: acc.hor + parseInt(curr[1]),
-                                depth: acc.depth + (parseInt(curr[1])*acc.aim)
+                                ...state,
+                                hor: state.hor + number,
+                                depth: state.depth + (number * state.aim)
                         };
                 case 'down':
-                        return {...acc, aim: acc.aim + parseInt(curr[1]) };
+                        return { ...state, aim: state.aim + number };
                 case 'up':
-                        return {...acc, aim: acc.aim - parseInt(curr[1]) };
+                        return { ...state, aim: state.aim - number };
                 default:
-                        throw "boem"
+                        return state;
         }
-        
-}, {depth: 0, hor: 0, aim: 0});
+}
 
 
-console.log(pos.depth , pos.hor, pos.depth * pos.hor);
+const endStatePart1 = commands.reduce(part1Reducer, defaultState);
+console.log(endStatePart1.depth * endStatePart1.hor);
+
+const endStatePart2 = commands.reduce(part2Reducer, defaultState);
+console.log(endStatePart2.depth * endStatePart2.hor);
+
+
